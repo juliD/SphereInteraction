3f03b4f31e42996ce3947d1f7bbc0e8d6aca54d7
Julia <ru76kim@mwn.eduroam.de>
2018-09-05 14:40:23 +0200

    initial commit

---
+++ Scripts/RotationInteraction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationInteraction : MonoBehaviour {
+
+    public GameObject small;
+    public GameObject big;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        big.transform.position = small.transform.position;
+        big.transform.rotation = new Quaternion(small.transform.rotation.x,
+                                            small.transform.rotation.y,
+                                            small.transform.rotation.z * -1.0f,
+                                            small.transform.rotation.w* -1.0f);
+    }
+}
