60faeab0e5f32ac3eb49b52d02c7d622cbb52ca8
Julia <ru76kim@mwn.eduroam.de>
2018-10-08 12:06:36 +0200

    4 scenarios with different views

--- Scripts/RotationInteraction_inv_turned.cs
+++ Scripts/RotationInteraction_inv_turned.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotationInteraction : MonoBehaviour {
+public class RotationInteraction_inv_turned : MonoBehaviour {
 
     public GameObject small;
     public GameObject big;
@@ -15,6 +15,9 @@ public class RotationInteraction : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         big.transform.position = small.transform.position;
-        big.transform.rotation = small.transform.rotation; 
+        big.transform.rotation = new Quaternion(small.transform.rotation.x ,
+            small.transform.rotation.y * -1,
+            small.transform.rotation.z,
+            small.transform.rotation.w) * Quaternion.Euler(0, 180f, 0); 
     }
 }
--- Scripts/RotationInteraction_turned.cs
+++ Scripts/RotationInteraction_turned.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotationInteraction : MonoBehaviour {
+public class RotationInteraction_turned : MonoBehaviour {
 
     public GameObject small;
     public GameObject big;
@@ -15,6 +15,8 @@ public class RotationInteraction : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         big.transform.position = small.transform.position;
-        big.transform.rotation = small.transform.rotation; 
-    }
-}
+        big.transform.rotation = new Quaternion(small.transform.rotation.x * -1,
+            small.transform.rotation.y,
+            small.transform.rotation.z,
+            small.transform.rotation.w) * Quaternion.Euler(0, 180f, 0); 
+    }}
