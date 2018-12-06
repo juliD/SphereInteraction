0f0c55c5029bd75b677c4bd22ccaa077f1308030
Julia <ru76kim@mwn.eduroam.de>
2018-12-06 08:35:37 +0100

    spherical interface

---
+++ Scripts/Scroll.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroll : MonoBehaviour {
+
+    public ScrollRect myScrollRect;
+    public Scrollbar newScrollBar;    
+    Vector3 oldRot;
+    
+
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float deltaRot = Vector3.SignedAngle(transform.forward, oldRot, transform.right);   
+        
+        if (deltaRot>0)
+        {
+            if (myScrollRect.verticalNormalizedPosition < 1)
+            { 
+                myScrollRect.verticalNormalizedPosition += deltaRot / 360;
+            } 
+        }
+        else
+        {
+            if (myScrollRect.verticalNormalizedPosition > 0)
+            {
+                myScrollRect.verticalNormalizedPosition += deltaRot / 360;
+            }           
+            
+        }
+        Debug.Log(oldRot + " "+ transform.forward);   
+        oldRot = transform.forward;
+        
+    }
+}
---
+++ Scripts/move_world.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_world : MonoBehaviour {
+
+    public GameObject camera;
+    Vector3 oldRot;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float deltaRot_Forward = Vector3.SignedAngle(transform.forward, oldRot, transform.right);
+        camera.transform.position -= camera.transform.forward * deltaRot_Forward * speed; 
+        
+        Debug.Log(deltaRot_Forward);
+        oldRot = transform.forward;
+    }
+}
